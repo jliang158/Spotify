58be2b010411836dd16543a76436b6a1509fc75a
Jonathan Liang <jonathanliang15@gmail.com>
2022-12-31 16:34:31 -0600

    Search bar works

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,12 +1,67 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 import useAuth from '../useAuth';
+import Box from '@mui/material/Box';
+import { FormControl, Input } from '@mui/material';
+import TrackSearchResult from './TrackSearchResult';
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: "ca720306eb134a42abd5463d72b5a1ff",
+})
+
+
 const Dashboard = (props: any) => {
   const {code} = props;
   const accessToken = useAuth(code)
+  const [search, setSearch] = useState('')
+  const [searchResults, setSearchResults] = useState([])
+  console.log(searchResults)
+
+  useEffect(() => {
+    if (!accessToken) return
+    spotifyApi.setAccessToken(accessToken)
+  }, [accessToken])
+
+  useEffect(() => {
+    if (!search) {
+      return setSearchResults([])
+    }
+    if (!accessToken) {
+      return
+    }
+    // let cancel = false;
+    spotifyApi.searchTracks(search).then((res: any) => {
+      // if (cancel) {
+      //   return
+      // }
+      setSearchResults(res.body.tracks.items.map((track: any) => {
+        const smallestAlbumImage = track.album.images.reduce((smallest: any, image: any) => {
+          if (image.height < smallest.height) return image
+          return smallest
+        }, track.album.images[0])
+        return {
+          artist: track.artists[0].name,
+          title: track.name,
+          uri: track.uri,
+          albumUrl: smallestAlbumImage.url
+        }
+      })
+      )
+    })
+    // cancel = true;
+    return;
+  }, [search, accessToken])
+
   return (
-    <div>
-      {code}
-    </div>
+    <Box className='Box'>
+      <FormControl>
+        <Input placeholder='Search Songs/Artists' onChange={(e) => {setSearch(e.target.value); console.log(e.target.value)}} value={search}></Input>
+      </FormControl>
+      <Box className='List'>{searchResults.map((track, i) => (
+        <TrackSearchResult track={track} key={i} />
+      ))}</Box>
+      <Box>Bottom</Box>
+    </Box>
   )
 }
 
---
+++ client/src/components/TrackSearchResult.tsx
@@ -0,0 +1,21 @@
+import { Box, width } from '@mui/system';
+import { useState, useEffect} from 'react';
+
+const TrackSearchResult = (props: any) => {
+  const { track } = props;
+
+  const handlePlay = () => {
+
+  }
+  return (
+    <Box className="TrackSearchResults">
+      <img src={track.albumUrl} style={{height:'64px', width:'64px'}} alt="images"/>
+      <Box className="TitleName">
+        <Box className="title">{track.title}</Box>
+        <Box className="artist">{track.artist}</Box>
+      </Box>
+    </Box>
+  )
+}
+
+export default TrackSearchResult;
