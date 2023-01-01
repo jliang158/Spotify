839e20a859769e3b09cbd5fd0e067bf1b3af520f
Jonathan Liang <jonathanliang15@gmail.com>
2022-12-31 21:27:24 -0600

    Finished

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const lyricsFinder = require('lyrics-finder')
 const SpotifyWebApi = require('spotify-web-api-node')
 const app = express();
 app.use(cors())
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/refresh', (req, res) => {
   const refreshToken = req.body.refreshToken
@@ -48,4 +50,8 @@ app.post('/login', (req, res) => {
   })
 })
 
+app.get('/lyrics', async (req, res) => {
+  const lyrics = await lyricsFinder(req.query.artist, req.query.track) || 'No Lyrics Found'
+  res.json({lyrics})
+})
 app.listen(3001)
